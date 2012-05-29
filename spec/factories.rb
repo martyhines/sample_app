06dbcbbdf73f9b9fd1fb9b6834f5a673dec01f90
Marty Hines <mhines76@gmail.com>
2012-05-28 23:26:42 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Vanessa Hines"
+    email    "nin_sandras@hotmail.com"
+    password "joshua14"
+    password_confirmation "joshua14"
+  end
+end
