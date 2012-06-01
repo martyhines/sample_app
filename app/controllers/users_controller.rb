f2aa1a82c7ee33812e3fe55f9a9ca87caead6a87
Marty Hines <mhines76@gmail.com>
2012-05-31 21:49:25 -0400

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,21 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+
   private
 
 
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -32,5 +32,13 @@ namespace :db do
       content = Faker::Lorem.sentence(5)
       users.each { |user| user.microposts.create!(:content => content) }
     end
+
+    users = User.all
+    user  = users.first
+    followed_users = users[2..50]
+    followers      = users[3..40]
+    followed_users.each { |followed| user.follow!(followed) }
+    followers.each      { |follower| follower.follow!(user) }
+
   end
 end
